73d5aff6d04b6c21e2d6abc1348c03011908df2b
Billy Peake <b.peake@illuscio.com>
2020-07-10 16:19:39 -0700

    added script to refactor python proto types

---
+++ py_make_proto.py
@@ -0,0 +1,33 @@
+import pathlib
+
+
+def fix_import_paths(python_file: pathlib.Path, pyi: bool):
+    file_text = python_file.read_text()
+    file_text = file_text.replace(
+        "from astral_proto", "from astral_grpc.protogen.astral_proto",
+    )
+    file_text = file_text.replace(
+        "import astral_proto", "import astral_grpc.protogen.astral_proto",
+    )
+
+    if not pyi:
+        file_text = file_text.replace(
+            "[astral_proto", "[astral_grpc.protogen.astral_proto",
+        )
+
+    file_text = file_text.replace(
+        " astral_proto.", " astral_grpc.protogen.astral_proto.",
+    )
+    python_file.write_text(file_text)
+
+
+def fix_generated_files():
+    for python_file in pathlib.Path("./astral_grpc/protogen").rglob("./**/*.py"):
+        fix_import_paths(python_file, pyi=False)
+
+    for python_file in pathlib.Path("./astral_grpc/protogen").rglob("./**/*.pyi"):
+        fix_import_paths(python_file, pyi=True)
+
+
+if __name__ == "__main__":
+    fix_generated_files()
